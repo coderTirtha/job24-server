afce9ab05db773d01471b2e9cd369c7387e19a85
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-11-10 00:00:28 +0600

    jwt verification added

--- index.js
+++ index.js
@@ -8,12 +8,33 @@ const app = express();
 const port = 5000 || process.env.PORT;
 
 // readyMade Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
-
-
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pj7mp5d.mongodb.net/?retryWrites=true&w=majority`;
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Not Authorized" })
+  }
+  jwt.verify(token, process.env.ACCESS_WEB_TOKEN, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send("Unauthorized")
+    }
+    // console.log('Value in the token', decoded);
+    req.user = decoded;
+    next();
+  });
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,11 +50,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const jobCollection = client.db('job24').collection('jobs');
-    app.post('/jobs', async(req, res) => {
-        const newJob = req.body;
-        console.log(newJob);
-        const result = await jobCollection.insertOne(newJob);
-        res.send(result);
+    // Authentication related APIs
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_WEB_TOKEN, { expiresIn: '1h' });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      }).send({ success: true });
+    });
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
+    // job CRUD related APIs
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      console.log(newJob);
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -47,9 +84,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("job24-server is running");
+  res.send("job24-server is running");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at PORT: ${port}`);
+  console.log(`Server is running at PORT: ${port}`);
 });
